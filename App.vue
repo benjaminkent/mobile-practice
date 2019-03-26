1c2f78e7599fd4122433c2460649299dd01f9cab
benjaminkent <benjamin.k.jehl@gmail.com>
2019-03-25 21:53:35 -0400

    inital commmit

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="container">
+    <Header />
+    <text class="text-color-primary">can I change stuff</text>
+  </view>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: "App",
+  components: { Header }
+}
+</script>
+
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.text-color-primary {
+  color: green;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <view class="header-container">
+    <text>does this work with html?</text>
+  </view>
+</template>
+
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
